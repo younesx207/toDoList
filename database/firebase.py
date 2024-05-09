23e0d683c602dcc4d2dadcee552d7b8939e47325
YounesKBE <122362381+younesx207@users.noreply.github.com>
2024-05-09 23:15:19 +0200

    firebase config update

--- database/firebase.py
+++ database/firebase.py
@@ -1,16 +1,40 @@
 import firebase_admin
+from firebase_admin import credentials
+import json
 import pyrebase
 
-from dotenv import dotenv_values
-import json
-env = dotenv_values(dotenv_path='.env')
+import os
 
-if not firebase_admin._apps :
-    cred = firebase_admin.credentials.Certificate(json.loads(env['FIREBASE_SERVICE_ACCOUNT_KEY']))
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+configs= {
+    "FIREBASE_SERVICE_ACCOUNT_KEY" :     os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY"),
+    "FIREBASE_CONFIG" : os.getenv('FIREBASE_CONFIG')
+              
+}
+
+
+firebase_config_json  =  json.loads(configs['FIREBASE_CONFIG'])
+
+
+# Parse the JSON key
+service_account_key_json = json.loads(configs["FIREBASE_SERVICE_ACCOUNT_KEY"])
+
+
+
+# import des crédentiels
+#initialise our app import credentials if not done
+if not firebase_admin._apps: 
+    cred = credentials.Certificate(service_account_key_json)
     firebase_admin.initialize_app(cred)
 
-firebase = pyrebase.initialize_app(json.loads(env['FIREBASE_CONFIG']))
-db = firebase.database()
 
-#authentication
+
+# getting access to firebase  by using rebase / create a new firebase instance  
+firebase = pyrebase.initialize_app(firebase_config_json)
+# access to our database instance by pyrebase 
+db = firebase.database()
 authTodo = firebase.auth()
